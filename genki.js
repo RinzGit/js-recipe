4cef63b24716cd92836c80164c76d560f67bcc8f
RinzGit <200fe632@st.nufs.ac.jp>
2023-04-12 21:14:26 +0900

    [３の倍数]を勉強しました。

---
+++ genki.js
@@ -0,0 +1,15 @@
+const number = 24
+if (number % 3 === 0) {
+  console.log(number + "!!!!!!")
+}
+
+const genkiFunction = function (number) {
+  for (let n = 1; n <= number; n++) {
+    if (n % 3 === 0) {
+      console.log(`${n}!!!!!!`)
+    } else {
+      console.log(n)
+    }
+  }
+}
+genkiFunction(24)
